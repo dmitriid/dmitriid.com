f070a6a425f7f0a58112f20cb3be0f3de63b9e60
Dmitrii Dimandt <dmitrii@dmitriid.com>
2017-02-07 14:07:39 +0100

    Use system hugo

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo "Running in $DIR"
 
 echo "::: Hugo server" && \
-~/.go/bin/hugo server --buildDrafts --theme=freja -v
+hugo server --buildDrafts --theme=freja -v

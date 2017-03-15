cda3791d941fde485c9027331f4e17d5905128da
Dmitrii Dimandt <dmitrii@dmitriid.com>
2017-03-15 15:12:14 +0100

    Run sass before running the site

--- run.sh
+++ run.sh
@@ -2,5 +2,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 echo "Running in $DIR"
 
+echo "::: SASS" && \
+sass --watch themes/freja/static/assets/sass/freja.scss:themes/freja/static/assets/css/freja.css & \
 echo "::: Hugo server" && \
 ~/.go/bin/hugo server --buildDrafts --theme=freja -v
